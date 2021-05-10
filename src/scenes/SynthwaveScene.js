052c975038d563e253e79945d0a2ec51638a9ddf
mbasith <meb@example.com>
2021-05-10 14:49:59 -0400

    updated with sockets

--- src/scenes/SynthwaveScene.js
+++ src/scenes/SynthwaveScene.js
@@ -3,6 +3,7 @@ import enemy from '../entity/Enemy';
 import gun from '../entity/Gun';
 import Ground from '../entity/Ground';
 import Laser from '../entity/Laser';
+import io from 'socket.io-client';
 
 const numberOfFrames = 15;
 
@@ -109,9 +110,18 @@ export default class SynthwaveScene extends Phaser.Scene {
 
     // Create game entities
     // << CREATE GAME ENTITIES HERE >>
-    this.player = new Player(this, 60, 400, 'josh').setScale(0.25);
+    this.player = new Player(this, 60, 400, 'josh', this.socket).setScale(0.25);
     this.player.setCollideWorldBounds(true); //stop player from running off the edges
     this.physics.world.setBounds(0, null, width * numberOfFrames, height, true, true, false, false) //set world bounds only on sides
+
+    //check other players moves and if collision between players:
+      this.socket.on("playerMoved", function (data){
+      
+      scene.otherPlayer.x = data.x
+      scene.otherPlayer.y = data.y
+      scene.otherPlayer.setPosition(data.x, data.y)
+      scene.physics.add.collider(scene.player, scene.otherPlayer, scene.processCollide);
+    })
 
     //set up camera
     const cam = this.cameras.main;
