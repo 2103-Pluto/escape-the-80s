ba8d6292c594ad65f0008eb281460339179032fe
Isaac Easton <iae1@protonmail.com>
2021-05-19 12:08:07 -0400

    Adds speech bubble to Level 1

--- src/scenes/SinglePlayerSynthwaveScene.js
+++ src/scenes/SinglePlayerSynthwaveScene.js
@@ -44,6 +44,7 @@ export default class SinglePlayerSynthwaveScene extends Phaser.Scene {
     this.createFlagpole = this.createFlagpole.bind(this)
     this.pause = this.pause.bind(this)
     this.createPhysics = this.createPhysics.bind(this)
+    this.createSpeechBubble = this.createSpeechBubble.bind(this)
   }
 
   init(data) {
@@ -403,6 +404,54 @@ export default class SinglePlayerSynthwaveScene extends Phaser.Scene {
       scene
     );
   }
+  
+  createSpeechBubble (x, y, width, height, quote, scene) {
+    const bubbleWidth = width;
+    const bubbleHeight = height;
+    const bubblePadding = 10;
+    const arrowHeight = bubbleHeight / 4;
+
+    let bubble = scene.add.graphics({ x: x, y: y });
+
+    //  Bubble shado
+    bubble.fillStyle(0x222222, 0.5);
+    bubble.fillRoundedRect(6, 6, bubbleWidth, bubbleHeight, 16);
+
+    //  Bubble color
+    bubble.fillStyle(0xffffff, 1);
+
+    //  Bubble outline line style
+    bubble.lineStyle(4, 0x565656, 1);
+
+    //  Bubble shape and outline
+    bubble.strokeRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+    bubble.fillRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+
+    //  Calculate arrow coordinates
+    const point1X = Math.floor(bubbleWidth / 7);
+    const point1Y = bubbleHeight;
+    const point2X = Math.floor((bubbleWidth / 7) * 2);
+    const point2Y = bubbleHeight;
+    const point3X = Math.floor(bubbleWidth / 7);
+    const point3Y = Math.floor(bubbleHeight + arrowHeight);
+
+    //  Bubble arrow shadow
+    bubble.lineStyle(4, 0x222222, 0.5);
+    bubble.lineBetween(point2X - 1, point2Y + 6, point3X + 2, point3Y);
+
+    //  Bubble arrow fill
+    bubble.fillTriangle(point1X, point1Y, point2X, point2Y, point3X, point3Y);
+    bubble.lineStyle(2, 0x565656, 1);
+    bubble.lineBetween(point2X, point2Y, point3X, point3Y);
+    bubble.lineBetween(point1X, point1Y, point3X, point3Y);
+
+    const content = this.add.text(0, 0, quote, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', wordWrap: { width: bubbleWidth - (bubblePadding * 2) } });
+
+    const b = content.getBounds();
+
+    content.setPosition(bubble.x + (bubbleWidth / 2) - (b.width / 2), bubble.y + (bubbleHeight / 2) - (b.height / 2));
+    return {bubble, content}
+}
 
   create() {
    // const scene = this
@@ -431,22 +480,33 @@ export default class SinglePlayerSynthwaveScene extends Phaser.Scene {
     this.pause(this) //creates pause functionality
     // --->
     const level1 = this.add.text(400, 300, 'LEVEL 1',{ fontFamily: '"Press Start 2P"' }).setFontSize(46).setOrigin(0.5, 0.5)
-    const flash = this.tweens.add({
+    const flashLevel1 = this.tweens.add({
       targets: level1,
       duration: 100,
       repeat: -1,
       tint: 0xffffff
-  })
+    })
   
   
-  this.time.addEvent({
-    delay: 1000,
-    callback: () => {
-      flash.stop()
-      level1.setVisible(false)
-    },
-    loop: false
-  })
+    this.time.addEvent({
+      delay: 1000,
+      callback: () => {
+        flashLevel1.stop()
+        level1.setVisible(false)
+      },
+      loop: false
+    })
+  
+    const speechBubble = this.createSpeechBubble(50, 300, 250, 110, "Holy crap, I'm in 1987! How did I get this gun???", this)
+
+    this.time.addEvent({
+      delay: 4000,
+      callback: () => {
+        speechBubble.content.setVisible(false)
+        speechBubble.bubble.setVisible(false)
+      },
+      loop: false
+    })
 
     this.cursors = this.input.keyboard.createCursorKeys();
     //this.physics.add.collider(this.player, this.platforms)
