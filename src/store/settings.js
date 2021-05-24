0000f13b987b0e22774aec67b24db07c4140d536
Isaac Easton <iae1@protonmail.com>
2021-05-24 14:25:50 -0400

    Adds black gunner easter egg

--- src/store/settings.js
+++ src/store/settings.js
@@ -1,5 +1,6 @@
 //ACTION CONSTANTS
 const SET_CAMPAIGN_DIFFICULTY = "SET_CAMPAIGN_DIFFICULTY"
+const SET_PLAYER_WON = "PLAYER_WON"
 
 //ACTION CREATORS
 export const setCampaignDifficulty = (difficulty) => {
@@ -9,16 +10,27 @@ export const setCampaignDifficulty = (difficulty) => {
   }
 }
 
+export const setPlayerVictory = (binary) => {
+  return {
+    type: SET_PLAYER_WON,
+    binary
+  }
+}
+
 //INITIAL STATE
 const initState = {
   campaignDifficulty: 'standard',
+  playerWon: false,
 }
+
 
 //SETTINGS REDUCER
 export default function recordsReducer (state = initState, action) {
   switch (action.type) {
     case SET_CAMPAIGN_DIFFICULTY:
       return Object.assign({}, state, {campaignDifficulty: action.difficulty})
+    case SET_PLAYER_WON:
+      return Object.assign({}, state, {playerWon: action.binary})
     default:
       return state;
   }
